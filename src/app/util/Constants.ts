ba3435c177598ce6f7a873b7a6e4a7676bd96c73
Robin1995 <robinson.rodriguez01@uptc.edu.co>
2019-02-18 00:37:15 -0500

    expresiones

--- src/app/util/Constants.ts
+++ src/app/util/Constants.ts
@@ -3,4 +3,5 @@ export class Constants {
   static readonly MINIMUM_YEARS_OF_STUDENT = 14;
   static readonly MAXIMUM_YEARS_OF_STUDENT = 100;
   static readonly regexOnlyLetterAndSpace = '^[a-zA-ZñÑ\\s]+$';
+  static readonly regexOnlyNumbers = '^[0-9]+$';
 }
